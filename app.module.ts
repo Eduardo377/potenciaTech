0bde036c22d4cb66b35220e35377c10e3541af83
Lorrayne <lorrayne.au@gmail.com>
2023-05-28 14:38:12 -0300

    Primeiro commit

---
+++ app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BodyHomeComponent } from './body-home/body-home.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [AppComponent, BodyHomeComponent, HeaderComponent, NavbarComponent, CardComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
